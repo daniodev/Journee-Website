e27497906d34ac37abe5fead1d74e18469326129
Paolo Puglia <paolo.puglia07@gmail.com>
2026-01-29 19:25:27 +0100

    Semplificato il FrontEnd e messi required
    messo un loop per creare i bottoni, messi anche i required agli input, frontend più platform friendly

--- Write.php
+++ Write.php
@@ -7,7 +7,7 @@
     <title>Journee Write</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-<?php
+    <?php
 $conn = mysqli_connect("localhost", "root", "", "journee_db") 
     or die("Connection failed: " . mysqli_connect_error());
 //$query = $conn -> query()
@@ -25,144 +25,66 @@ echo"Password sb";
 
 ?>
 
-    </head>
+</head>
 
 <body>
-    <div class="container-fluid">
+    <form action="Sendin.php" method="post">
+        <div class="container-fluid">
 
-        <div class="row">
-
-            <h1>How was Today?<span class="badge text-bg-secondary">New</span></h1>
-        </div>
-    
-        <div class="row">
-        
-            <div class="col-9">
-
-                <textarea id="comments" name="comments" rows="7" cols="50">
-                    Write your thoughts
-                </textarea>
+            <div class="row">
+                <h1>How was Today?</h1>
             </div>
 
-            <div class="col-1">
+            <div class="row">
 
-                <h4>Scala 1</h4>
-                <div class="form-check">
-                    <input class="form-check-input" type="Radio" name="ValueDefault" id="ValueDefault5">
-                    <label class="form-check-label" for="ValueDefault5">
-                        5
-                    </label>
+                <div class="col-md-6 col-12">
+
+                    <textarea class="form-control" id="comments" name="comments" rows="7" cols="50"
+                        placeholder="Write your thoughts" required></textarea>
                 </div>
-                <div class="form-check">
-                    <input class="form-check-input" type="Radio" name="ValueDefault" id="ValueDefault4" checked>
-                    <label class="form-check-label" for="ValueDefault4">
-                        4
-                    </label>
+
+                <div class="col-md-2 col-4">
+
+                    <h4>Scala 1</h4>
+                    <?php for($i=5;$i>=1;$i--): ?>
+                    <div class="form-check">
+                        <input class="form-check-input" type="Radio" name="scale1" value="<?= $i ?>"
+                            <?php if($i==5) echo "required"; ?>>
+                        <label class="form-check-label"><?= $i ?></label>
+                    </div>
+                    <?php endfor; ?>
                 </div>
-                <div class="form-check">
-                    <input class="form-check-input" type="Radio" name="ValueDefault" id="ValueDefault3" checked>
-                    <label class="form-check-label" for="ValueDefault3">
-                        3
-                    </label>
+
+                <div class="col-md-2 col-4">
+
+                    <h4>Scala 2</h4>
+                    <?php for($i=5;$i>=1;$i--): ?>
+                    <div class="form-check">
+                        <input class="form-check-input" type="Radio" name="scale2" value="<?= $i ?>"
+                            <?php if($i==5) echo "required"; ?>>
+                        <label class="form-check-label"><?= $i ?></label>
+                    </div>
+                    <?php endfor; ?>
                 </div>
-                <div class="form-check">
-                    <input class="form-check-input" type="Radio" name="ValueDefault" id="ValueDefault2" checked>
-                    <label class="form-check-label" for="ValueDefault2">
-                        2
-                    </label>
-                </div>
-                <div class="form-check">
-                    <input class="form-check-input" type="Radio" name="ValueDefault" id="ValueDefault1" checked>
-                    <label class="form-check-label" for="ValueDefault1">
-                        1
-                    </label>
+
+
+                <div class="col-md-2 col-4">
+
+                    <h4>Scala 3</h4>
+                    <?php for($i=5;$i>=1;$i--): ?>
+                    <div class="form-check">
+                        <input class="form-check-input" type="Radio" name="scale3" value="<?= $i ?>"
+                            <?php if($i==5) echo "required"; ?>>
+                        <label class="form-check-label"><?= $i ?></label>
+                    </div>
+                    <?php endfor; ?>
                 </div>
             </div>
-
-
-            <div class="col-1">
-
-                <h4>Scala 2</h4>
-                <div class="form-check">
-                    <input class="form-check-input" type="Radio" name="ValueDefault2" id="ValueDefault5">
-                    <label class="form-check-label" for="ValueDefault5">
-                        5
-                    </label>
-                </div>
-                <div class="form-check">
-                    <input class="form-check-input" type="Radio" name="ValueDefault2" id="ValueDefault4" checked>
-                    <label class="form-check-label" for="ValueDefault4">
-                        4
-                    </label>
-                </div>
-                <div class="form-check">
-                    <input class="form-check-input" type="Radio" name="ValueDefault2" id="ValueDefault3" checked>
-                    <label class="form-check-label" for="ValueDefault3">
-                        3
-                    </label>
-                </div>
-                <div class="form-check">
-                    <input class="form-check-input" type="Radio" name="ValueDefault2" id="ValueDefault2" checked>
-                    <label class="form-check-label" for="ValueDefault2">
-                        2
-                    </label>
-                </div>
-                <div class="form-check">
-                    <input class="form-check-input" type="Radio" name="ValueDefault2" id="ValueDefault1" checked>
-                    <label class="form-check-label" for="ValueDefault1">
-                        1
-                    </label>
-                </div>
-
-            </div>
-
-            <div class="col-1">
-                <h4>Scala 3</h4>
-                <div class="form-check">
-                    <input class="form-check-input" type="Radio" name="ValueDefault3" id="ValueDefault5">
-                    <label class="form-check-label" for="ValueDefault5">
-                        5
-                    </label>
-                </div>
-                <div class="form-check">
-                    <input class="form-check-input" type="Radio" name="ValueDefault3" id="ValueDefault4" checked>
-                    <label class="form-check-label" for="ValueDefault4">
-                        4
-                    </label>
-                </div>
-                <div class="form-check">
-                    <input class="form-check-input" type="Radio" name="ValueDefault3" id="ValueDefault3" checked>
-                    <label class="form-check-label" for="ValueDefault3">
-                        3
-                    </label>
-                </div>
-                <div class="form-check">
-                    <input class="form-check-input" type="Radio" name="ValueDefault3" id="ValueDefault2" checked>
-                    <label class="form-check-label" for="ValueDefault2">
-                        2
-                    </label>
-                </div>
-                <div class="form-check">
-                    <input class="form-check-input" type="Radio" name="ValueDefault3" id="ValueDefault1" checked>
-                    <label class="form-check-label" for="ValueDefault1">
-                        1
-                    </label>
-                </div>
-
-            </div>
-    
         </div>
 
-    </div>
-    <button onclick="SendinFunction()" type="button" class="btn">Base class</button>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous">
-        function SendinFunction(){
-        }
-    </script>
-<?php
-
-?>
+        <button type="submit">Invia</button>
+    </form>
 </body>
+
 </html>
