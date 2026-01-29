7e42bb9e1d6521137cce38b7f3e17c23d33ded83
Paolo Puglia <paolo.puglia07@gmail.com>
2026-01-29 09:59:11 +0100

    Base View
    Creazione Pagina di Scrittura

---
+++ Write.php
@@ -0,0 +1,103 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Journee Write</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container">
+        <h1>How was Today?<span class="badge text-bg-secondary">New</span></h1>
+    </div>
+    <div class="container">
+    
+        <div class="container">
+            <textarea id="comments" name="comments" rows="4" cols="50">
+            Write your thoughts
+            </textarea>
+        </div>
+
+        <div class="container">
+            <h4>Scala 1</h4>
+            <div class="form-check">
+                <input class="form-check-input" type="Radio" name="ValueDefault" id="ValueDefault5">
+                <label class="form-check-label" for="ValueDefault5">
+                    5
+                </label>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" type="Radio" name="ValueDefault" id="ValueDefault4" checked>
+                <label class="form-check-label" for="ValueDefault4">
+                    4
+                </label>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" type="Radio" name="ValueDefault" id="ValueDefault3" checked>
+                <label class="form-check-label" for="ValueDefault3">
+                    3
+                </label>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" type="Radio" name="ValueDefault" id="ValueDefault2" checked>
+                <label class="form-check-label" for="ValueDefault2">
+                    2
+                </label>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" type="Radio" name="ValueDefault" id="ValueDefault1" checked>
+                <label class="form-check-label" for="ValueDefault1">
+                    1
+                </label>
+            </div>
+        </div>
+
+
+        <div class="container">
+            <h4>Scala 2</h4>
+            <div class="form-check">
+                <input class="form-check-input" type="Radio" name="ValueDefault2" id="ValueDefault5">
+                <label class="form-check-label" for="ValueDefault5">
+                    5
+                </label>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" type="Radio" name="ValueDefault2" id="ValueDefault4" checked>
+                <label class="form-check-label" for="ValueDefault4">
+                    4
+                </label>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" type="Radio" name="ValueDefault2" id="ValueDefault3" checked>
+                <label class="form-check-label" for="ValueDefault3">
+                    3
+                </label>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" type="Radio" name="ValueDefault2" id="ValueDefault2" checked>
+                <label class="form-check-label" for="ValueDefault2">
+                    2
+                </label>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" type="Radio" name="ValueDefault2" id="ValueDefault1" checked>
+                <label class="form-check-label" for="ValueDefault1">
+                    1
+                </label>
+            </div>
+        </div>
+    
+    
+</div>
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
